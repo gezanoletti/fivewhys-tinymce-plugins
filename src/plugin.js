de2f6e0747537a8b3d38f2c716f0d33794d6eb7e
Guillermo Zanoletti <gzanoletti@kaizen.com.ni>
2017-09-12 16:11:38 -0600

    implementation

--- src/plugin.js
+++ src/plugin.js
@@ -1,24 +1,34 @@
 import _ from 'lodash';
 
 const plugin = (editor) => {
-  editor.addButton('fivewhys', {
-    text: 'fivewhys',
-    icon: false,
-    onclick: () => {
-      // Open window
-      editor.windowManager.open({
-        title: 'fivewhys plugin',
-        body: [
-          { type: 'textbox', name: 'title' }
-        ],
-        onsubmit(e) {
-          // Insert content when the window form is submitted
-          const kebabbyString = _.kebabCase(e.data.title);
-          editor.insertContent(kebabbyString);
-        }
-      });
-    }
-  });
+	editor.addButton('fivewhys', {
+		text: 'Five Whys',
+		icon: false,
+		onclick: () => {
+			// Open window
+			editor.windowManager.open({
+				width: 600,
+				height: 230,
+				title: 'Five Whys',
+				body: [
+					{type: 'textbox', name: 'w1', label: 'First Why'},
+					{type: 'textbox', name: 'w2', label: 'Second Why'},
+					{type: 'textbox', name: 'w3', label: 'Third Why'},
+					{type: 'textbox', name: 'w4', label: 'Fourth Why'},
+					{type: 'textbox', name: 'w5', label: 'Fifth Why'},
+				],
+				onsubmit(e) {         
+				  editor.insertContent("<ul>");
+				  editor.insertContent("<li>" + e.data.w1 + "</li>");
+				  editor.insertContent("<li>" + e.data.w2 + "</li>");
+				  editor.insertContent("<li>" + e.data.w3 + "</li>");
+				  editor.insertContent("<li>" + e.data.w4 + "</li>");
+				  editor.insertContent("<li>" + e.data.w5 + "</li>");
+				  editor.insertContent("</ul>");
+				}
+			});
+		}
+	});
 };
 
 export default plugin;
